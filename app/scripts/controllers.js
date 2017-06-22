3092f5f7c402442c583c7c45d9a85b145dabecae
Suvigya Vijay <suvigyavijay@gmail.com>
2017-06-22 10:45:29 +0530

    save

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -96,7 +96,8 @@ angular.module('lenestApp')
 			    var originalContents = document.body.innerHTML;
 			    document.body.innerHTML = printContents;
 			    window.print();
-			    document.body.innerHTML = originalContents;
+			    // document.body.innerHTML = originalContents;
+			    window.location.reload(false);
 			} 
 
 			function genPrintOutput() {
@@ -306,7 +307,7 @@ angular.module('lenestApp')
 		    var originalContents = document.body.innerHTML;
 		    document.body.innerHTML = printContents;
 		    window.print();
-		    document.body.innerHTML = originalContents;
+		    window.location.reload(false);
 		}
 
     }])
